e4c34eb8405e0461fda3c8663a68ade682a965fc
mailin <dreamywheatml@hotmail.com>
2022-08-12 18:04:40 +0800

    update

--- src/main/java/cn/thinkingdata/generate/code/service/XMLMetaService.java
+++ src/main/java/cn/thinkingdata/generate/code/service/XMLMetaService.java
@@ -1,5 +1,6 @@
 package cn.thinkingdata.generate.code.service;
 
+import cn.thinkingdata.generate.GenUtils;
 import cn.thinkingdata.generate.code.domain.EntryDO;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
@@ -7,6 +8,7 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -17,24 +19,26 @@ public class XMLMetaService {
     private static int index = 0;
 
     HashMap<String, LinkedList<EntryDO>> headMaps = new HashMap();
-    LinkedList<EntryDO> ClanHead;
-    LinkedList<EntryDO> PlayerHead;
-    LinkedList<EntryDO> ServerHead;
 
     public LinkedList<EntryDO> getAllEntries(String structName) throws DocumentException {
-        LinkedList<EntryDO> entryList = new LinkedList<EntryDO>();
+        LinkedList<EntryDO> entryList = new LinkedList<>();
         parserXml(structName, entryList);
         return entryList;
     }
 
     public void init() throws DocumentException {
-        ClanHead = getAllEntries("head/ClanHead.xml");
-        PlayerHead = getAllEntries("head/PlayerHead.xml");
-        ServerHead = getAllEntries("head/ServerHead.xml");
-
-        headMaps.put("ClanHead", ClanHead);
-        headMaps.put("PlayerHead", PlayerHead);
-        headMaps.put("ServerHead", ServerHead);
+        File[] files = (new File("src/main/resources/head")).listFiles();
+        for (File file : files) {
+            if (file.getName().endsWith(".xml")) {
+                try {
+                    String filename = file.getName();
+                    LinkedList<EntryDO> entries = getAllEntries("head/" + filename);
+                    headMaps.put(filename.substring(0, filename.indexOf(".xml")), entries);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void parserXml(String fileName, LinkedList<EntryDO> entryList) throws DocumentException {
